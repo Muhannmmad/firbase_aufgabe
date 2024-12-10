668a54fd733a3a7bc53404c2425b033b177d6712
Muhammad <muhannmmad@gmail.com>
2024-12-10 15:29:51 +0100

    code updated to work WidgetsFlutterBinding.ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:firbase_aufgabe/firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MainApp());
 }
 

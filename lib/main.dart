993ff26009268a6c587270fb5f15725eca82c206
Muhammad <muhannmmad@gmail.com>
2024-12-12 15:24:46 +0100

    firestore made hobbies collection

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,59 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firbase_aufgabe/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MainApp());
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: Hobbies(),
+    );
+  }
+}
+
+class Hobbies extends StatelessWidget {
+  const Hobbies({Key? key}) : super(key: key);
+
+  Future<List<Map<String, dynamic>>> fetchHobbies() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collectionGroup('Hobbies').get();
+
+    return querySnapshot.docs.map((doc) {
+      return {
+        'id': doc.id,
+        ...doc.data() as Map<String, dynamic>,
+      };
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchHobbies(),
+        builder: (context, snapshot) {
+          final hobbies = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: hobbies.length,
+            itemBuilder: (context, index) {
+              final hobby = hobbies[index];
+              return ListTile(
+                title: Text(hobby['name']),
+                subtitle: Text(
+                  'Type: ${hobby['type']}, Members: ${hobby['members']}',
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
